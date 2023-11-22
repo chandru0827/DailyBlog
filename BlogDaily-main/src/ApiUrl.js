8022a560a77f7ab57069aaa069189b0aebbd481c
chandru0827 <chandru0827@gmail.com>
2023-11-22 16:00:06 +0530

    task1

---
+++ BlogDaily-main/src/ApiUrl.js
@@ -0,0 +1,3 @@
+export const GETPOSTS = "https://chandru0827.pythonanywhere.com/getPosts";
+export const CREATEPOST = "https://chandru0827.pythonanywhere.com/addPost";
+export const GETPOST = (id) => `https://chandru0827.pythonanywhere.com/getPost/${id}`;
---
+++ BlogDaily-main/src/Helpers/Theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const themes = createTheme({
+  palette: {
+    primary: {
+      main: "#ffc8c4",
+      white: "#ffffff",
+      bgColor: "#ffffff7e",
+      darkbtn: "#ea3333",
+      maindark: "#ffffff",
+      dark: "#e04848",
+      greydark: "#ffffff",
+      offwhite: "#000000",
+    },
+  },
+});
